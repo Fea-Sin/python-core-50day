949b556e595c2979041e502a6aa0f2c633453be4
Fea-Sin <1210992614@qq.com>
2020-11-05 18:38:49 +0800

    python ch1

---
+++ src/ch1/hello.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time   : 2020/11/5 6:32 下午
+# @Author : FEASIN
+
+"""
+第一个Python程序 - hello world
+
+Version: 0.1
+"""
+
+# print('hello world')
+print('hello world')
